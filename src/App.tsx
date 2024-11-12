82c33614c29ca770456622b291ab0dd95f70e550
marat <unclemarat@gmail.com>
2024-11-12 17:03:04 +0300

    New init

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import './App.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home, Login, FriendList, Register, Profile } from './pages';
-import { Layout } from './components/Layout/Layout';
-import { useAppDispatch } from './redux/redux-hook';
-import { useEffect } from 'react';
-import { fetchLoginMe } from './redux/slices/login';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(fetchLoginMe());
-  }, [dispatch]);
+  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/friends' element={<FriendList />} />
-          <Route path='/:userId' element={<Profile />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/hooks/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useAppSelector } from '../redux/redux-hook';
-
-export const useAuth = () => {
-  const isAuth = useAppSelector((state) => state.auth.data);
-  return Boolean(isAuth);
-};
